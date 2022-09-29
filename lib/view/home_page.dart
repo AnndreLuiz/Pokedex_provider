f3e42fc56a905da57a3bb99edae62eb04cd0a744
AnndreLuiz <andre.aqrt@gmail.com>
2022-09-29 14:34:29 -0300

    finish

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -4,7 +4,6 @@ import '../model/PokemonJson.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
 
-
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -19,7 +18,6 @@ class _HomePageState extends State<HomePage> {
 
   @override
   void initState() {
-
     presenter = context.read<PokemonPresenter>();
     presenter.getPokemons();
     super.initState();
@@ -32,7 +30,7 @@ class _HomePageState extends State<HomePage> {
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-           const Text('Pokedex'),
+            const Text('Pokedex'),
             SizedBox(
               width: 35,
               child: Image.network(
@@ -42,53 +40,52 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
-      body: Consumer<PokemonPresenter>(
-        builder: (_, pokemons , widget) {
-          return ListView.builder(
-            itemCount: pokemons.pokemonsList.length,
-            itemBuilder: (context, index) {
-              return Card(
-                margin: const EdgeInsets.all(5),
-                child: InkWell(
-                  onTap: () {
-                    Navigator.of(context).pushNamed('/descricao',arguments: pokemons.pokemonsList[index]);
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.all(9.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Image.network(
-                          '${pokemons.pokemonsList[index].thumbnailImage}',
-                          width: 95,
-                          height: 95,
-                        ),
-                        Column(
-                          children: [
-                            Text(
-                              pokemons.pokemonsList[index].name ?? '',
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 17,
-                              ),
+      body: Consumer<PokemonPresenter>(builder: (_, pokemons, widget) {
+        return ListView.builder(
+          itemCount: pokemons.pokemonsList.length,
+          itemBuilder: (context, index) {
+            return Card(
+              margin: const EdgeInsets.all(5),
+              child: InkWell(
+                onTap: () {
+                  Navigator.of(context).pushNamed('/descricao',
+                      arguments: pokemons.pokemonsList[index]);
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(9.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Image.network(
+                        '${pokemons.pokemonsList[index].thumbnailImage}',
+                        width: 95,
+                        height: 95,
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            pokemons.pokemonsList[index].name ?? '',
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17,
                             ),
-                          ],
-                        ),
-                        const SizedBox(
-                          width: 125,
-                        ),
-                        Text(
-                          '#${pokemons.pokemonsList[index].number ?? ''}',
-                        ),
-                      ],
-                    ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        width: 125,
+                      ),
+                      Text(
+                        '#${pokemons.pokemonsList[index].number ?? ''}',
+                      ),
+                    ],
                   ),
                 ),
-              );
-            },
-          );
-        }
-      ),
+              ),
+            );
+          },
+        );
+      }),
     );
   }
 }
